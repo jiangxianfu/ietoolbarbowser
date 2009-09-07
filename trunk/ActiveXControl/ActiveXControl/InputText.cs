98b311f92d0e990310aca62e938e7ec3ee62c8ce
jiangxianfu <jiangxianfu@33fe2be0-9add-11de-8acd-13b7c682a6e5>
2009-09-07 00:59:28 +0000

    实现网页中activex控件

---
+++ trunk/ActiveXControl/ActiveXControl/InputText.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ActiveXControl
+{
+    [Guid("9D0CA446-0293-43d2-B033-8BDF8D9619ED")]
+    public partial class InputText : UserControl,IObjectSafety
+    {
+        public InputText()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this.txtInput.Text);
+        }
+
+        #region IObjectSafety Members
+
+        public void GetInterfacceSafyOptions(int riid, out int pdwSupportedOptions, out int pdwEnabledOptions)
+        {
+            pdwSupportedOptions = 1;
+            pdwEnabledOptions = 2;
+        }
+
+        public void SetInterfaceSafetyOptions(int riid, int dwOptionsSetMask, int dwEnabledOptions)
+        {
+            //throw new NotImplementedException();
+        }
+
+        #endregion
+        public void HelloWorld()
+        {
+            MessageBox.Show("Hello World");
+        }
+        public string GetHelloWorld()
+        {
+            return "Hello World" + this.txtInput.Text;
+        }
+    }
+}
---
+++ trunk/ActiveXControl/WebApp/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApp._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <object id="Test_Ctrl" 
+    classid="clsid:9D0CA446-0293-43d2-B033-8BDF8D9619ED"
+    codebase="steup.exe"
+    width="1000px" height="20px" name="aaa">
+    </object>
+    </div>
+    </form>
+</body>
+</html>
